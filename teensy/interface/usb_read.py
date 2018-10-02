b8b3fbd8f69b29fbfd9c87a17d89fe782ac6d638
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-10-02 18:18:10 -0400

    Revamp interface to grab the correct data frames

--- teensy/interface/usb_read.py
+++ teensy/interface/usb_read.py
@@ -4,6 +4,7 @@ import serial
 import matplotlib.pyplot as plt
 
 ser = serial.Serial('/dev/ttyACM0')
+ser.flushInput()
 np.set_printoptions(threshold=np.inf)
 
 # See https://stackoverflow.com/questions/7100242/python-numpy-first-occurrence-of-subarray
@@ -19,7 +20,7 @@ def rolling_window(a, size):
 # Returns:
 # - nelem: the number of elements from that subarray
 # - partial_array: a flag that tells us if we need to get data at the start of the next frame
-def read_buffer(data,start_frame):
+def read_buffer(data,start_frame,partial_array):
     # Read a line and interpret as uint8
     line = ser.read(BUF_SIZE)
     current = np.frombuffer(line,dtype=np.uint8)
@@ -28,24 +29,24 @@ def read_buffer(data,start_frame):
     bool_indices = np.all(rolling_window(current, 32) == start_frame, axis=1)
     array_idx = np.mgrid[0:len(bool_indices)][bool_indices]
 
-    # Get the data from the transmitted buffer, but skip the SOT 32 bytes 
-    start_idx = array_idx[0] + 32
-
-    # How many elements is that?
-    nelem = len(current[start_idx::])
-
-    # If we have less data than the total length of data required
-    # then copy the data from the buffer, and flag that the array is not full yet 
-    if nelem < NELEM_BYTES:
-        np.copyto(data[0:nelem],current[start_idx::])
-        partial_array = True
-
-    # Otherwise copy 6200 elements and discard the garbage
-    else:
-        np.copyto(data,current[start_idx:start_idx+NELEM_BYTES])
+    # TODO: Add check to see if bool_indices is empty or not. Maybe try/catch?
+    if partial_array:
+        end_idx = array_idx[0]
+        nelem = len(current[0:end_idx])
+        print("Partial = True",array_idx,end_idx,nelem)
+        np.copyto(data[len(data)-nelem-32:len(data)-32],current[0:end_idx])
         partial_array = False
+
+    else:
+        start_idx = array_idx[-1] + 32
+        nelem = len(current[start_idx::])
+        partial_array = True
+        try:
+            np.copyto(data[0:nelem],current[start_idx::])
+        except:
+            np.copyto(data,current[start_idx::])
         
-    return nelem,partial_array
+    return nelem, partial_array
 
 ### CONSTANTS
 # Start frame
@@ -63,75 +64,52 @@ ADC_RES = 4095 # ADC resolution (12 bits)
 ADC_V = 5 # ADC operating voltage (5 V)
 
 # Holders for data
-data = np.zeros(NELEM_BYTES,dtype=np.uint8)
+data = np.zeros(BUF_SIZE,dtype=np.uint8) # Data for second half of packet
+data_p1 = np.zeros(BUF_SIZE,dtype=np.uint8) # Data for first half of packet
 
-nelem, partial_array = read_buffer(data,start_frame)
+data16 = data[0:6200].view(dtype=np.uint16) # This is of size 3100 pixels
+data16_p1 = data_p1[0:6200].view(dtype=np.uint16)
 
-data16 = data.view(dtype=np.uint16) # This is of size 3100 pixels
 data_V = np.zeros(NELEM,dtype=np.float64) # This is of size 3100 pixels
+data_plot = np.zeros(len(data16),dtype=np.float64)
+data_plot_p1 = data_plot
 
-# Averaging quantities
-NAVE = 10 # Number of frame averages
-data_ave = np.zeros(NELEM,dtype=np.float64) 
-idx = 0 
-
-### PLOT
+#### PLOT
 fig = plt.figure()
 ax = fig.add_subplot(111)
-
-li, = ax.plot(data16)
-li_ave, = ax.plot(data_ave)
+data_plot = 1.0*data16 + 1.0*data16_p1
+li, = ax.plot(data_plot,'.')
+li_p1, = ax.plot(data_plot_p1,'x')
 
 # Draw and show it
 ax.relim() 
 ax.autoscale_view(True,True,True)
 fig.canvas.draw()
 plt.show(block=False)
-
 plt.ylim([0,5])
-plt.xlim([0,NELEM])
 
-
-
+pa = False # Partial array?
 while True:
-    # If we had a partial array, then we grab the next buffer line and fill the rest with the beginning of the array
-    if(partial_array):
-        line = ser.read(BUF_SIZE)
-        current = np.frombuffer(line,dtype=np.uint8)
+#for idx in np.arange(0,1,1):
+    nelem, pa = read_buffer(data,start_frame,pa)
+    nelem, pa = read_buffer(data_p1,start_frame,pa)
 
-        # How many elements did we miss?
-        nadd = NELEM_BYTES-nelem
-        np.copyto(data[nelem::],current[0:nadd])
-        
-        data16 = data.view(dtype=np.uint16)
-        partial_array = False
-    # Otherwise, the previous data was fine, so we can now try to fill the buffer again
-    else:
-        #print(data16,np.max(data16),data16.size,nelem,partial_array)
-        # Convert to voltage and plot
-        data_V = ADC_V / ADC_RES * data16 
-        print(data16[0:100])
+    data16 = data[0:6200].view(dtype=np.uint16) # This is of size 3100 pixels
+    data16_p1 = data_p1[0:6200].view(dtype=np.uint16)
 
-        li.set_ydata(data_V)
-        fig.canvas.draw()
+    #data_plot = 1.0*data16 + 1.0*data16_p1
+    data_plot = 1.0*data16 
+    data_plot_p1 = 1.0*data16_p1
+    data_plot *= ADC_V / ADC_RES
+    data_plot_p1 *= ADC_V / ADC_RES
 
-        # Accumulate the averaged data if we have not reached the number of samples
-        if idx < NAVE:
-            data_ave += data_V
-            idx = idx + 1
-        # Otherwise, calculate actual average and update plot
-        else:
-            idx = 0
-            data_ave = data_ave / NAVE
-            li_ave.set_ydata(data_ave)
-            print(np.average(data_ave))
-            data_ave = np.zeros(NELEM,dtype=np.float64) 
-    
-        # Reinitialize our data holder
-        data = np.zeros(NELEM_BYTES,dtype=np.uint8)
-        data16 = np.zeros(NELEM,dtype=np.uint16)
-        nelem,partial_array = read_buffer(data,start_frame)
-        data16 = data.view(dtype=np.uint16)
+    data = np.zeros(BUF_SIZE,dtype=np.uint8) # Data for second half of packet
+    data_p1 = np.zeros(BUF_SIZE,dtype=np.uint8) # Data for first half of packet
 
-#    time.sleep(0.1)
+    li.set_ydata(data_plot)
+    li_p1.set_ydata(data_plot_p1)
+    fig.canvas.draw()
+
+
+plt.show()
 
