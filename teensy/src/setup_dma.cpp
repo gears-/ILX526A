d2ffa4d9201fea8cb5d502af5d65d22019b2d35c
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-10-04 11:31:22 -0400

    Preface the files with the AGPLv3 license

--- teensy/src/setup_dma.cpp
+++ teensy/src/setup_dma.cpp
@@ -1,3 +1,25 @@
+/* ILX526A Teensy Interface v1.0: a C interface running on the Teensy v3.2 to 
+ * obtain data from an ILX526A CCD that is digitized with an ADS803U ADC.
+ *
+ * Copyright (C) 2018 Pierre-Yves Taunay
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact info: https://github.com/pytaunay
+ *
+ * Source: https://github.com/pytaunay/ILX526A
+ */
 #include "setup_dma.h"
 /* 
  *  Function: setup_dma
