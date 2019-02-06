cb523323c55772e94d87d0ac99f2d5240e2b611b
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-02-06 09:29:33 -0500

    Remove useless code

--- teensy/interface/spectrointerface/gui/SpectroGraph.py
+++ teensy/interface/spectrointerface/gui/SpectroGraph.py
@@ -25,30 +25,7 @@ from spectrointerface.gui.MplCanvas import MplCanvas
 from PyQt5 import QtCore
 
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Cursor
 import numpy as np
-
-class Cursor(object):
-    def __init__(self, ax, canvas):
-        self.ax = ax
-        self.canvas = canvas
-        self.lx = ax.axhline(color='k')  # the horiz line
-        self.ly = ax.axvline(color='k')  # the vert line
-
-        # text location in axes coords
-        self.txt = ax.text(0.7, 0.9, '', transform=ax.transAxes)
-
-    def mouse_move(self, event):
-        if not event.inaxes:
-            return
-
-        x, y = event.xdata, event.ydata
-        # update the line positions
-        self.lx.set_ydata(y)
-        self.ly.set_xdata(x)
-
-        self.txt.set_text('x=%1.2f, y=%1.2f' % (x, y))
-        self.canvas.draw()
 
 class SnaptoCursor(object):
     """
